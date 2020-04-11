f4bfcb06bd119f884275883476bd3fa62268ea86
gameplan1975 <gameplan1975@gmail.com>
2020-04-11 13:34:07 +0000

    画像のリサイズ、indexの降順処理

--- app/models/blog.rb
+++ app/models/blog.rb
@@ -2,4 +2,5 @@ class Blog < ApplicationRecord
     belongs_to :user
     mount_uploader :blog_image, BlogImageUploader
     validates :content, presence: true
+    default_scope -> { order(updated_at: :asc) }
 end
--- app/uploaders/blog_image_uploader.rb
+++ app/uploaders/blog_image_uploader.rb
@@ -1,4 +1,6 @@
 class BlogImageUploader < CarrierWave::Uploader::Base
+  include CarrierWave::MiniMagick
+  process :resize_to_limit => [480, 320]
   storage :file
   def store_dir
     "uploads/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
